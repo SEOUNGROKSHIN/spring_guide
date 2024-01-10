7da0f3ae938073f89412869ed550ba60ee398829
SHINSEOUNGROK <tnsfhr03@gmail.com>
2024-01-10 21:15:31 +0900

    상세 페이지 구현완ㅇ료

--- src/main/java/com/guideservice/domain/web/form/FormItemController.java
+++ src/main/java/com/guideservice/domain/web/form/FormItemController.java
@@ -6,22 +6,43 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/form/items")
 public class FormItemController {
 
     private final ItemRepository itemRepository;
 
-    @GetMapping("/form/items")
+    @GetMapping()
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
-        model.addAttribute("items" , items);
-
+        model.addAttribute("items", items);
         return "form/items";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "form/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/form/items/{itemId}";
+    }
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId , Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+    return "form/item";
     }
 }
